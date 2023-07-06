50a0cf21af5ecefdc1bbae242730e57f76e45ebf
NjeriMarion <njerimarion@gmail.comgit config --global user.email njerimarion@gmail.comhistorygit clone https://github.com/NjeriMarion/tools_new_2.git>
2023-07-06 07:25:44 +0300

    react APIs

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from './login';
+import Products from './products';
+
+function App() {
+  return (
+    <div className="App">
+      <Login/>
+      <Products/>
+    </div>
+  );
+}
+
+export default App;
